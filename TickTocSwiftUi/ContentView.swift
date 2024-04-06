39389ff9071c1c1099754ac6c3dec0812c66905b
mithileshssingh <mithilesh.singh@blackbuck.com>
2024-04-06 12:07:05 +0530

    Initial Commit

---
+++ TickTocSwiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TickTocSwiftUi
+//
+//  Created by Mithilesh Singh on 06/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TickTocSwiftUi/TickTocSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTocSwiftUiApp.swift
+//  TickTocSwiftUi
+//
+//  Created by Mithilesh Singh on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TickTocSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
